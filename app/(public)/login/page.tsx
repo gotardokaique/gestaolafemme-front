67e5bc4bc910bca31d2a717ba88611084a89dc45
Kaique Gotardo <kaiquecgotardo@gmail.com>
2026-02-06 18:12:58 -0400

    feat: add public login page with background image and form.

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -4,15 +4,13 @@ export default function LoginPage() {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       {/* Background Image */}
-      <div 
+      <div
         className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: 'url(/img/bg-telafundo.jpg)' }}
+        style={{ backgroundImage: 'url(/img/bg-telafundov1.jpg)' }}
       />
-      
-      {/* Overlay para melhorar legibilidade */}
+
       <div className="absolute inset-0 z-0 bg-black/20 dark:bg-black/40" />
-      
-      {/* Content */}
+
       <div className="relative z-10 w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
